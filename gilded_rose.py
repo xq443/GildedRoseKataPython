674b8ce3f607ad25cb6cc61ca9a46413f9c42997
Cathy Qin <qin.xuj@northeastern.edu>
2024-10-08 20:52:48 -0700

    refactor with behavioural pattern

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,49 +1,78 @@
 # -*- coding: utf-8 -*-
 
-
+"""
+Refactor the source code with Behavioural Pattern to encapsulate the behavior for different types of items.
+e.g., regular items, Aged Brie, Backstage Passes, and Sulfuras.
+This approach will simplify the update_quality method by letting each item type handle its own specific behavior in separate classes.
+"""
 class Item:
-    """ DO NOT CHANGE THIS CLASS!!!"""
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
 
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class UpdateStrategy:
+    """Base strategy class for updating items."""
+    def update(self, item: Item):
+        raise NotImplementedError("This method should be overridden by subclasses")
 
 
-class GildedRose(object):
+# inherit from the base class UpdateStrategy
+class RegularItemStrategy(UpdateStrategy):
+    """Strategy for regular items."""
+    def update(self, item: Item):
+        item.sell_in -= 1
+        if item.quality > 0:
+            item.quality -= 1
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality -= 1
 
+
+class AgedBrieStrategy(UpdateStrategy):
+    """Strategy for Aged Brie."""
+    def update(self, item: Item):
+        item.sell_in -= 1
+        if item.quality < 50:
+            item.quality += 1
+        if item.sell_in < 0 and item.quality < 50:
+            item.quality += 1
+
+
+class BackstagePassesStrategy(UpdateStrategy):
+    """Strategy for Backstage Passes."""
+    def update(self, item: Item):
+        item.sell_in -= 1
+        if item.quality < 50:
+            item.quality += 1
+            if item.sell_in < 10 and item.quality < 50:
+                item.quality += 1
+            if item.sell_in < 5 and item.quality < 50:
+                item.quality += 1
+        if item.sell_in < 0:
+            item.quality = 0
+
+
+class SulfurasStrategy(UpdateStrategy):
+    """Strategy for Sulfuras, Hand of Ragnaros (Legendary Item)."""
+    def update(self, item: Item):
+        # Sulfuras does not change in sell_in or quality.
+        pass
+
+
+class GildedRose:
+    """Context class that uses strategies to update items."""
     def __init__(self, items: list[Item]):
-        # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
+        # align with the test cases
+        self.strategies = {
+            "Aged Brie": AgedBrieStrategy(),
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePassesStrategy(),
+            "Sulfuras, Hand of Ragnaros": SulfurasStrategy(),
+        }
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            # get strategy of the item, if no specific strategy, then use RegularItemStrategy() as default one
+            strategy = self.strategies.get(item.name, RegularItemStrategy()) 
+            strategy.update(item)
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
 import unittest
-
 from gilded_rose import Item, GildedRose
 
 
@@ -9,67 +7,47 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("foo_fail", items[0].name)
-        
-        
-    def test_vest_item_should_decrease_after_one_day(self):
+        self.assertEqual("foo", items[0].name)
 
+    def test_vest_item_should_decrease_after_one_day(self):
         vest = "+5 Dexterity Vest"
-        items = [Item(vest, 1, 2), Item(vest, 9, 19), Item(vest, 4, 6), ]
+        items = [Item(vest, 1, 2), Item(vest, 9, 19), Item(vest, 4, 6)]
+        gr = GildedRose(items)
+        gr.update_quality()
+
+        self.assertEqual(items[0].sell_in, 0)
+        self.assertEqual(items[0].quality, 1)
+        self.assertEqual(items[1].sell_in, 8)
+        self.assertEqual(items[1].quality, 18)
+        self.assertEqual(items[2].sell_in, 3)
+        self.assertEqual(items[2].quality, 5)
+
+    def test_aged_brie_increases_in_quality(self):
+        name = "Aged Brie"
+        items = [Item(name, 2, 0)]
         gr = GildedRose(items)
 
         gr.update_quality()
-        
         self.assertEqual(items[0].sell_in, 1)
-        self.assertEqual(items[0].quality,2)
-        self.assertEqual(items[1].sell_in, 9)
-        self.assertEqual(items[1].quality, 19)
-        self.assertEqual(items[2].sell_in, 4)
-        self.assertEqual(items[2].quality, 6)
-        
+        self.assertEqual(items[0].quality, 1)
 
-        # or just write a enumerate loop to test
-        # expected_items = [Item(vest, 0, 1), Item(vest, 8, 18), Item(vest, 3, 5)]
-        # for i, item in enumerate(gr.items):
-        #     assert item.name == expected_items[i].name
-        #     assert item.sell_in == expected_items[i].sell_in
-        #     assert item.quality == expected_items[i].quality
-        
-        
-    def test_aged_brie_increases_in_quality(self):
-        name = "Aged Brie"
-        items = [Item(name, 2, 0)]  # Item starts with sell_in = 2 days, quality = 0
-        gr = GildedRose(items)
+        gr.update_quality()
+        self.assertEqual(items[0].sell_in, 0)
+        self.assertEqual(items[0].quality, 2)
 
-        # Call update_quality for 3 days to check the progression
-        gr.update_quality()  # Day 1
-        self.assertEqual(items[0].sell_in, 2)  # 1
-        self.assertEqual(items[0].quality, 0)  # Quality increases by 1
-
-        gr.update_quality()  # Day 2
-        self.assertEqual(items[0].sell_in, 1)  # 0
-        self.assertEqual(items[0].quality, 1)  # Quality increases by 1 again : 2
-
-        gr.update_quality()  # Day 3 (sell_in < 0)
-        self.assertEqual(items[0].sell_in, 0) # -1
-        self.assertEqual(items[0].quality, 0)  # Quality increases by 2 after sell_in < 0: 4
+        gr.update_quality()
+        self.assertEqual(items[0].sell_in, -1)
+        self.assertEqual(items[0].quality, 4)
 
     def test_sulfuras_hand_of_agnaros_in_quality(self):
         name = "Sulfuras, Hand of Ragnaros"
-        items = [Item(name, 2, 0), Item(name, 12, 0), Item(name, 22, 0)]  # Item starts with sell_in = 2 days, quality = 0
+        items = [Item(name, 0, 80)]
         gr = GildedRose(items)
-        
-        # Update quality for 5 days
-        for _ in range(5):
-            gr.update_quality()
-            
-        for item in items:
-            assert item.quality == 1 # Quality does not change by sell_in days
-            
-        assert items[0].sell_in == 12
-        assert items[1].sell_in == 22
-        assert items[2].sell_in == 102 # Sell_in does not change by sell_in days
-            
-        
+        gr.update_quality()
+
+        self.assertEqual(items[0].sell_in, 0)
+        self.assertEqual(items[0].quality, 80)
+
+
 if __name__ == '__main__':
     unittest.main()
